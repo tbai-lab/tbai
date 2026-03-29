e7e1819becc19453a4bdd590924a65d7042e4c5a
lnotspotl <lnotspotl@gmail.com>
2026-03-29 16:45:53 +0200

    remove unnecessary _C import from __init__.py

--- tbai_python/tbai/__init__.py
+++ tbai_python/tbai/__init__.py
@@ -1,6 +1,5 @@
 from tbai._C import *  # noqa: F401, F403
 from tbai._C import rotations  # noqa: F401
-from tbai import _C  # noqa: F401
 
 # Fix repr so classes show as tbai.X instead of tbai._C.X
 for _obj in list(vars().values()):
